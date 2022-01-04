98507fa99857c481563575503c8c4c38015cb458
David <36444115+dawissl@users.noreply.github.com>
2022-01-04 09:02:32 +0100

    ddd

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi!");
+            Console.WriteLine("Hi!v World!");
         }
     }
 }
